572a60431afc3f17852c847f0e25483f7543c556
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-18 13:08:53 +0100

    too much examples

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { HookUseToggleExample } from './components/HookUseToggleExample';
 import { HookUsePrevExample } from './components/HookUsePrevExample';
 import { HookUseIntervalExample } from './components/HookUseIntervalExample';
 import './App.css';
-
+//uncomment false to see hook in action
 function App() {
   return (
     <div className="App">
-      <HookUseToggleExample />
-      <HookUsePrevExample />
+      {false && <HookUseToggleExample />}
+      {false && <HookUsePrevExample /> }
       {false && <HookUseIntervalExample/>}
     </div>
   );
