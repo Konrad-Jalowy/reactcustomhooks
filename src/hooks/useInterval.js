aab9dff0af94c443b7cfec495272dc8f22748c29
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-18 13:00:15 +0100

    skeleton

--- src/hooks/useInterval.js
+++ src/hooks/useInterval.js
@@ -1,5 +1,11 @@
 import { useEffect, useRef } from "react";
 
 export function useInterval(callback, delay){
+
+    const callbackRef = useRef();
+
+    useEffect(() => {
+        callbackRef.current = callback;
+    }, [callback])
     return "not implemented";
 };
