2a092816fe5db36fac0b58de85ddf5944a26e269
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-18 12:23:44 +0100

    useprev skeleton

--- src/hooks/usePrev.js
+++ src/hooks/usePrev.js
@@ -1,6 +1,10 @@
 import { useRef } from "react";
 
-export function usePrev(){
-    return "not implemented";
+export function usePrev(reactiveVariable){
+
+    const currentValue = useRef(reactiveVariable);
+    const previousValue = useRef();
+    //TODO: LOGIC!!!
+    return previousValue.current;
 };
 
