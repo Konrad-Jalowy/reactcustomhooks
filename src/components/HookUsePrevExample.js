5db5a1ee640bab0583d6a8660cde8f3444658e9c
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-18 12:27:08 +0100

    working on useprev example

--- src/components/HookUsePrevExample.js
+++ src/components/HookUsePrevExample.js
@@ -1,6 +1,9 @@
 import { usePrev } from "../hooks/usePrev";
+import { useState } from "react";
 
 function HookUsePrevExample(){
+    const [count, setCount] = useState(0);
+    const prev = usePrev(count);
     return "not implemented";
 };
 
