02683cc6fafebbee3fa4024741fbe4ee59c9ae40
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-19 09:35:53 +0100

    usearray hook

--- src/hooks/useArray.js
+++ src/hooks/useArray.js
@@ -1,5 +1,33 @@
 import { useState } from "react";
 
 export default function useArray(defaultValue){
-    return "not implemented";
+
+    const [arr, setArr] = useState(defaultValue);
+
+    function push(el){
+        setArr(prev => [...prev, el]);
+    }
+
+    function filter(callback){
+        setArr(a => a.filter(callback));
+    }
+
+    function update(index, newElement) {
+        setArr(a => [
+          ...a.slice(0, index),
+          newElement,
+          ...a.slice(index + 1, a.length),
+        ]);
+      }
+    
+      function remove(index) {
+        setArr(a => [...a.slice(0, index), ...a.slice(index + 1, a.length)]);
+      }
+    
+      function clear() {
+        setArr([]);
+      }
+
+
+      return { arr, set: setArr, push, filter, update, remove, clear };
 };
