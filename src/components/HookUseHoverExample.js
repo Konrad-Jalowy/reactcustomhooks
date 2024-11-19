cb5318bb3fcff9dfa40762d5ca92cd21653ac9ed
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-19 13:43:52 +0100

    useHover example skeleton

---
+++ src/components/HookUseHoverExample.js
@@ -0,0 +1,8 @@
+import { useRef } from "react";
+import { useHover } from "../hooks/useHover";
+
+function HookUseHoverExample(){
+    return "not implemented";
+}
+
+export {HookUseHoverExample};
--- src/hooks/useHover.js
+++ src/hooks/useHover.js
@@ -2,6 +2,24 @@ import { useState, useEffect } from "react";
 
 export const useHover = (ref) => {
   
+    const [isHovered, setIsHovered] = useState(false);
 
-  return "not implemented";
+    const handleMouseEnter = () => setIsHovered(true);
+    const handleMouseLeave = () => setIsHovered(false);
+  
+    useEffect(() => {
+      const node = ref.current;
+  
+      if (node) {
+        node.addEventListener("mouseenter", handleMouseEnter);
+        node.addEventListener("mouseleave", handleMouseLeave);
+  
+        return () => {
+          node.removeEventListener("mouseenter", handleMouseEnter);
+          node.removeEventListener("mouseleave", handleMouseLeave);
+        };
+      }
+    }, [ref]);
+  
+    return isHovered;
 };
